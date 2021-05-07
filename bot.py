0330496f5f4cecde386dbcb20af941042587154a
dynamite14 <78571139+dynamite14@users.noreply.github.com>
2021-05-06 20:47:06 -0400

    add ping and hello commands

--- bot.py
+++ bot.py
@@ -10,7 +10,18 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix='c!')
-client = Client()
+client = discord.Client(intents=discord.Intents.all())
+slash = SlashCommand(bot, sync_commands=True)
+
+testServers = [738488607261851748]
+
+@slash.slash(name="ping")
+async def _ping(ctx):
+    await ctx.send(f"Pong! ({client.latency*1000}ms)")
+
+@slash.slash(name="hello", guild_ids=testServers)
+async def hello(ctx):
+    await ctx.send("Hello Joe!")
 
 @bot.command(name='senkobread')
 async def senko(ctx):
@@ -22,7 +33,7 @@ async def embedtest(ctx):
     colorcode = int("0x%02x%02x%02x" % (random.randint(0,255), random.randint(0,255), random.randint(0,255)), 16)
 
     testembed = Embed(title="Test Title",description="Test Description",color=colorcode)
-    
+
     await ctx.send(embed=testembed)
 
 bot.run(TOKEN)
