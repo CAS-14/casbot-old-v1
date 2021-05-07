15a45f239741ecff08a465fda000d5ed96dc0cab
dynamite14 <78571139+dynamite14@users.noreply.github.com>
2021-05-06 21:23:08 -0400

    formatting

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ slash = SlashCommand(bot, sync_commands=True)
 # 738488607261851748 Awesome Realm Official
 # 674791516249653277 CAS Testing Server
 # 814158378653712455 Chez Cult Remastered
-testServers = [738488607261851748,674791516249653277,814158378653712455]
+testServers = [738488607261851748, 674791516249653277, 814158378653712455]
 
 @slash.slash(name="ping")
 async def _ping(ctx):
