14187011cad3ad190a734c513d71f93062d5dc57
dynamite14 <78571139+dynamite14@users.noreply.github.com>
2021-05-07 08:24:20 -0400

    clean up code and add status, remove slash commands

--- bot.py
+++ bot.py
@@ -11,9 +11,9 @@ from discord_slash.utils.manage_commands import create_option
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-versionName = "v26"
+gameStatus = "raid shadow legends"
 
-activity = Game(name=versionName)
+activity = Game(name=gameStatus)
 # activity = Streaming(name="!help", url="twitch_url_here")
 # activity = Activity(type=ActivityType.listening, name="!help")
 # activity = Activity(type=ActivityType.watching, name="!help")
@@ -26,25 +26,14 @@ slash = SlashCommand(client, sync_commands=True)
 # 674791516249653277 CAS Testing Server
 testServers = [738488607261851748, 674791516249653277]
 
-@slash.slash(name="ping")
-async def _ping(ctx):
-    await ctx.send(f"Pong! ({bot.latency*1000}ms)")
-
-@slash.slash(name="hello", guild_ids=testServers, description="Say hello to Joe")
-async def hello(ctx):
-    await ctx.send("Hello Joe!")
-
 @bot.command(name='senkobread')
 async def senko(ctx):
     await ctx.send("https://cdn.discordapp.com/emojis/795829520162357298.png?v=1")
 
 @bot.command(name='embedtest')
 async def embedtest(ctx):
-
     colorcode = int("0x%02x%02x%02x" % (random.randint(0,255), random.randint(0,255), random.randint(0,255)), 16)
-
     testembed = Embed(title="Test Title",description="Test Description",color=colorcode)
-
     await ctx.send(embed=testembed)
 
 bot.run(TOKEN)
