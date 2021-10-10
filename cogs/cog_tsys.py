8b1817a2357e62a6d789ed438d642c55d4584676
dynamite14 <78571139+dynamite14@users.noreply.github.com>
2021-10-10 12:16:07 -0400

    re-engineer tsys s

--- cogs/cog_tsys.py
+++ cogs/cog_tsys.py
@@ -6,8 +6,8 @@ import cogs.config as config
 URL_1 = 'https://canary.discord.com/api/webhooks/896608021432631316/20LF9S9UFd5oV86J-tx6LdSMaP71PsOKzjtn69_AAg9uc4twZIb3It4JjJd1kVXdJzNv'
 webhook1 = Webhook.from_url(URL_1, adapter=RequestsWebhookAdapter())
 
-# 879364514255077447 dynamite14
-# 814158379207491616 CC3 general
+tsys_USER = 786448912587948052      # dynamite14
+tsys_GENERAL = 814158379207491616   # CC3 general
 
 class TSys(commands.Cog):
     def __init__(self, bot):
@@ -24,13 +24,13 @@ class TSys(commands.Cog):
 
     @commands.command()
     async def tsys_sendserver(self, ctx):
-        channel = self.bot.get_channel(814158379207491616) # CC3 general
+        channel = self.bot.get_channel(tsys_GENERAL) # CC3 general
         await channel.send('**tSys** TO SERVER TEST: Hello World! :hamburger:')
 
     # forward DMs from target to webhook
     @commands.Cog.listener()
     async def on_message(self, message: Message):
-        if message.author.id == 786448912587948052 and message.guild.id is None and not message.author.bot: # dynamite14 user ID
+        if message.author.id == tsys_USER and message.guild.id is None and not message.author.bot: # dynamite14 user ID
             try:
                 webhook1.send(message.content)
             except Exception as err:
@@ -39,12 +39,14 @@ class TSys(commands.Cog):
     # forward all messages from CC3 to DMs
     @commands.Cog.listener()
     async def on_message(self, message: Message):
-        if message.channel.id == 814158379207491616 and not message.author.bot: # CC3 general
+        if message.channel.id == tsys_GENERAL and not message.author.bot: # CC3 general
             try:
-                channel = self.bot.get_channel(879364514255077447) # dynamite14 x casbot DM
-                embed = Embed(title="toriSys", color=message.author.color, description=message.content)
-                embed.set_author(name=message.author.name, icon_url=message.author.avatar_url)
-                embed.set_footer(text="{message.channel.name} in {message.guild.name}", icon_url=message.guild.icon_url)
+                target_user = self.bot.get_user(tsys_USER) # dynamite14
+
+                tsys_frd = Embed(title="toriSys", color=message.author.color, description=message.content)
+                tsys_frd.set_author(name=message.author.name, icon_url=message.author.avatar_url)
+                tsys_frd.set_footer(text="{message.channel.name} in {message.guild.name}", icon_url=message.guild.icon_url)
+                target_user.send(embed=tsys_frd)
             except Exception as err:
                 message.channel.send(f":x: **Error:**\n```{err}\n```")
 
