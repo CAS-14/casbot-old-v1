6fb438d93d4afbebdf2a29caa1c6e6778b777d4e
dynamite14 <78571139+dynamite14@users.noreply.github.com>
2021-10-10 11:51:04 -0400

    cleanup start script

--- bot.py
+++ bot.py
@@ -1,11 +1,9 @@
-# bot.py
 import os
 from discord import *
 from discord.ext import commands
 import cogs.config as config
 
 TOKEN = os.getenv('DISCORD_TOKEN')
-# TOKEN = "MANUAL OVERRIDE"
 
 # activity = Game(name=gameStatus)
 # activity = Streaming(name="c!help", url="twitch_url_here")
@@ -13,13 +11,6 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 # activity = Activity(type=ActivityType.watching, name="!help")
 
 bot = commands.Bot(command_prefix=config.prefix, status=Status.online)
-# client = Client(intents=Intents.all())
-
-"""
-extensions = ['admin', 'core', 'test']
-for ext in extensions:
-    bot.load_extension('cogs.cog_'+ext)
-"""
 
 verbose_start = True
 
@@ -32,8 +23,8 @@ async def on_ready():
     for ext in exts:
         try:
             bot.load_extension('cogs.cog_'+ext)
-            await bt_channel.send(':white_checkmark: Cog extension `{ext}` loaded successfully!') if verbose_start else None
+            await bt_channel.send(f':white_checkmark: Cog extension `{ext}` loaded successfully!') if verbose_start else None
         except Exception as err:
-            await bt_channel.send(':warning: Could not load cog extension`{ext}`. Error:\n{err}')
+            await bt_channel.send(f':warning: Could not load cog extension`{ext}`. Error:\n{err}')
 
 bot.run(TOKEN)
