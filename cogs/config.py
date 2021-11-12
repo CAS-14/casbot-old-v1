d2c534a2b3a59d97a6e62eab1419cc2d59a980ae
dynamite14 <78571139+dynamite14@users.noreply.github.com>
2021-11-12 16:07:45 -0500

    add runcode command and setup free storage system, improve permcheck

--- cogs/config.py
+++ cogs/config.py
@@ -1,5 +1,17 @@
 # Make sure to update all values in bot.py
 
+def checkMaster(userid):
+    if userid in bot_masters:
+        return True
+    else:
+        return False
+
+def checkOwner(userid):
+    if userid in bot_owners:
+        return True
+    else:
+        return False
+
 prefix = 'c!'
 
 testServers = [895356775941939262] # BOT-TESTING
@@ -10,6 +22,9 @@ bot_masters = [786448912587948052]
 channel_ids = {'BT-casbot'          : 895359225553907792,
                'CC3-general'        : 814158379207491616,
                'CC3-bot-commands'   : 814158830392311838}
+
+free_storage = {'CHANNEL' : 896612054423703583,
+                'msg_main' : None}
 
 testvar = "cheezburger"
 
