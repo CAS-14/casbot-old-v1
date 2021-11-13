54bc1b055a905cf06e132ce2e728af099c7bf06d
dynamite14 <78571139+dynamite14@users.noreply.github.com>
2021-11-13 15:20:10 -0500

    add currency

---
+++ cogs/cog_currency.py
@@ -0,0 +1,36 @@
+from discord import *
+from discord.ext import commands
+import cogs.cb_util as util
+from firebase_admin import db
+
+def register(userid):
+        ref = db.reference("/currency/")
+        if userid in ref.get():
+            return "User already registered"
+        ref.update({userid: {"balance":1000}})
+
+def bset(userid, bal):
+    db.reference(f"/currency/{userid}").update({"balance":bal})
+
+def bget(userid):
+    return db.reference(f"/currency/{userid}/balance/").get()
+
+class Test(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def bal(self, ctx):
+        await ctx.send(":moneybag: Your balance is $"+str(bget(ctx.author.id)))
+
+    @commands.command()
+    async def gain(self, ctx, amount):
+        if not util.checkMaster(ctx.author.id):
+            await ctx.send(":x: Access denied. You must be a **Bot Master** to use this command.")
+            return
+        
+        bset(ctx.author.id, bget(ctx.author.id)+amount)
+        await ctx.send(f":money_with_wings: Added ${amount} to your balance. You now have ${bget(ctx.author.id)}")
+
+def setup(bot):
+    bot.add_cog(Test(bot))
